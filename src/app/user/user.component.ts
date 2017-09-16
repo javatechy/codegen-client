294f3b20bf09cdbbc92000d942dee27fe76af890
Deepak <deepak@Deepaks-MacBook-Pro.local>
2017-09-16 18:45:10 +0530

    User Balance added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,48 @@
-import {Component, Input, ChangeDetectorRef, OnInit, AfterViewInit, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {LoginService} from '../services/login.service';
 import {HelperService} from '../services/helper.service';
-
+import {FormService} from '../services/form.service';
+import * as AppUtils from '../utils/app.utils';
+import {Common} from '../utils/Common';
+import {Balance, Transaction, User} from '../model/CustomResponse';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  providers: [ HelperService],
+  providers: [HelperService],
 })
-export class UserComponent {
 
+export class UserComponent implements OnInit {
+  user: User;
+  balance: Balance;
+  walletBalance: Number;
+
+
+  constructor(private router: Router, private loginService: LoginService, private route: ActivatedRoute,
+              public form: FormBuilder, private helperService: HelperService, private formService: FormService) {
+  }
+
+  ngOnInit() {
+    if (Common.getStorage(AppUtils.LS_USER_ROLE) === AppUtils.ROLE_NON_ADMIN) {
+      this.getBalance(Common.getStorage(AppUtils.LS_USER_ID));
+    } else {
+      this.helperService.openSnackBar('Please login again!!');
+    }
+  }
+
+  getBalance(userId: number) {
+    this.helperService.get(AppUtils.BACKEND_API_BALANCE + userId).subscribe(customResponse => {
+      if (customResponse.status === AppUtils.BE_STATUS_SUCCESS) {
+        this.walletBalance = customResponse.balance.balance;
+        this.helperService.openSnackBar('Balance Is :' + customResponse.balance.balance);
+      } else {
+        this.helperService.openSnackBar('Error While fetching balance : ' + customResponse.error.message);
+      }
+    });
+  }
 }
+
+
