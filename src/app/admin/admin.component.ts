a54b280ed2de5b1c15be033196e8608ecabbe589
Deepak <deepak@Deepaks-MacBook-Pro.local>
2017-09-16 17:55:32 +0530

    List of users added

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,6 +6,9 @@ import {HelperService} from '../services/helper.service';
 import {FormService} from '../services/form.service';
 import * as AppUtils from '../utils/app.utils';
 import {Common} from '../utils/Common';
+import {User} from '../model/CustomResponse';
+import 'rxjs/add/observable/of';
+
 
 @Component({
   selector: 'app-admin',
@@ -13,6 +16,9 @@ import {Common} from '../utils/Common';
   providers: [HelperService],
 })
 export class AdminComponent implements OnInit {
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  users: User[];
+
   constructor(private router: Router, private loginService: LoginService, private route: ActivatedRoute,
               public form: FormBuilder, private helperService: HelperService, private formService: FormService) {
   }
@@ -21,6 +27,9 @@ export class AdminComponent implements OnInit {
     if (Common.getStorage(AppUtils.LS_USER_ROLE) === AppUtils.ROLE_ADMIN) {
       this.helperService.get(AppUtils.BACKEND_API_USERS).subscribe(customResponse => {
         if (customResponse.status === AppUtils.BE_STATUS_SUCCESS) {
+          this.users = customResponse.users;
+          this.helperService.openSnackBar(JSON.stringify(customResponse.users));
+
         } else {
           this.helperService.openSnackBar('Error While fetching users list : ' + customResponse.error.message);
         }
